9162b04f5c70290d0e45c3e539b104bc258d9644
RaghavRR <raghavrastogi2021@gmail.com>
2026-01-20 01:28:51 +0530

    Enable POST and CORS in proxy

--- api/proxy/[...path].js
+++ api/proxy/[...path].js
@@ -1,4 +1,17 @@
+export const config = {
+  api: {
+    bodyParser: true,
+  },
+};
+
 export default async function handler(req, res) {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,OPTIONS");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  if (req.method === "OPTIONS") {
+    return res.status(200).end();
+  }
+
   try {
     const backendURL = "http://178.16.139.140:8080";
 
